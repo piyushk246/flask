8aae7a1161a3286ed0cdab19405de73faccfea8d
piyushk246 <65733681+piyushk246@users.noreply.github.com>
2023-11-06 17:36:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def index():
     return render_template('test.html')
 @app.route('/h')
 def index():
-    return hello dear
+    return "hello dear"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=false)
