b1e37c0cf430405eaa130cab7609ede580fa90ff
piyushk246 <65733681+piyushk246@users.noreply.github.com>
2023-11-06 17:34:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/h')
+@app.route('/')
 def index():
     return render_template('test.html')
-
+@app.route('/h')
+def index():
+    return hello dear
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=false)
