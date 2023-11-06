4a33729724c728a25ee5b559b14ad3da534482e4
piyushk246 <65733681+piyushk246@users.noreply.github.com>
2023-11-06 18:00:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from io import BytesIO
 import base64
 app = Flask(__name__)
 
+
+        
+@app.route('/')
+def index():
+    return render_template('test.html')
+    
 def load_data_from_json(battery_no):
     file_path = f"battery_data/battery{battery_no}.json"
     try:
@@ -13,13 +19,6 @@ def load_data_from_json(battery_no):
         return data
     except (FileNotFoundError, json.JSONDecodeError):
         return []
-        
-@app.route('/')
-def index():
-    return render_template('test.html')
-# @app.route('/h')
-# def index1():
-#     return "hello dear"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=false)
