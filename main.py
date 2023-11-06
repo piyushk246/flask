79b68c1887fdf40bba1cc493da2f6b411f9767a7
piyushk246 <65733681+piyushk246@users.noreply.github.com>
2023-11-06 18:05:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 def index():
     return render_template('test.html')
     
-def load_data_from_json(battery_no):
-    file_path = f"battery_data/battery{battery_no}.json"
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-        return data
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
+# def load_data_from_json(battery_no):
+#     file_path = f"battery_data/battery{battery_no}.json"
+#     try:
+#         with open(file_path, 'r') as file:
+#             data = json.load(file)
+#         return data
+#     except (FileNotFoundError, json.JSONDecodeError):
+        # return []
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=false)
